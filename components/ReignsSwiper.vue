88bff287f8466d5338f9d0ee87ba16c9db3ac119
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-07-05 12:28:09 -0400

    Update styles

--- components/ReignsSwiper.vue
+++ components/ReignsSwiper.vue
@@ -117,24 +117,26 @@
           </g>
         </g>
       </svg>
-      <svg width="32px" height="39px" viewBox="0 0 32 39" version="1.1" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink">
-        <title>icon-retry</title>
-        <g id="UX-Flow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <g id="Scenerio-00---00" transform="translate(-302, -738)">
-            <g id="icon-retry" transform="translate(302, 738)">
-              <text id="RETRY" font-family="Roboto-Medium, Roboto" font-size="10" font-weight="400" fill="#fff">
-                <tspan x="0.851074219" y="37">RETRY</tspan>
-              </text>
-              <g id="arrow-retry" transform="translate(4, 0)" fill="#fff" fill-rule="nonzero">
-                <path
-                  d="M13.4918439,0 C7.75606199,-0.000796072417 3.07928399,4.5944661 2.98368767,10.325 L1.42497783,8.96875 C1.05989384,8.67492329 0.527988496,8.7205009 0.21829939,9.07214718 C-0.0913897156,9.42379347 -0.0690052759,9.95676748 0.269080645,10.28125 L3.29893236,12.90625 C3.62205979,13.1716525 4.08791813,13.1716525 4.41104556,12.90625 L7.79116915,10.28125 C8.07415324,9.9627877 8.08657068,9.48699187 7.82058256,9.15422457 C7.55459444,8.82145728 7.08746406,8.72837963 6.71408314,8.93375 L4.73504705,10.5 C4.73504705,5.66750844 8.65559855,1.75 13.4918439,1.75 C18.3280893,1.75 22.2486408,5.66750844 22.2486408,10.5 C22.2486408,15.3324916 18.3280893,19.25 13.4918439,19.25 C13.0082194,19.25 12.6161642,19.6417508 12.6161642,20.125 C12.6161642,20.6082492 13.0082194,21 13.4918439,21 C19.2953383,21 24,16.2989898 24,10.5 C24,4.70101019 19.2953383,0 13.4918439,0 Z"
-                  id="Path"></path>
+      <a href="/" class="cursor-pointer">
+        <svg width="32px" height="39px" viewBox="0 0 32 39" version="1.1" xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink">
+          <title>icon-retry</title>
+          <g id="UX-Flow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Scenerio-00---00" transform="translate(-302, -738)">
+              <g id="icon-retry" transform="translate(302, 738)">
+                <text id="RETRY" font-family="Roboto-Medium, Roboto" font-size="10" font-weight="400" fill="#fff">
+                  <tspan x="0.851074219" y="37">RETRY</tspan>
+                </text>
+                <g id="arrow-retry" transform="translate(4, 0)" fill="#fff" fill-rule="nonzero">
+                  <path
+                    d="M13.4918439,0 C7.75606199,-0.000796072417 3.07928399,4.5944661 2.98368767,10.325 L1.42497783,8.96875 C1.05989384,8.67492329 0.527988496,8.7205009 0.21829939,9.07214718 C-0.0913897156,9.42379347 -0.0690052759,9.95676748 0.269080645,10.28125 L3.29893236,12.90625 C3.62205979,13.1716525 4.08791813,13.1716525 4.41104556,12.90625 L7.79116915,10.28125 C8.07415324,9.9627877 8.08657068,9.48699187 7.82058256,9.15422457 C7.55459444,8.82145728 7.08746406,8.72837963 6.71408314,8.93375 L4.73504705,10.5 C4.73504705,5.66750844 8.65559855,1.75 13.4918439,1.75 C18.3280893,1.75 22.2486408,5.66750844 22.2486408,10.5 C22.2486408,15.3324916 18.3280893,19.25 13.4918439,19.25 C13.0082194,19.25 12.6161642,19.6417508 12.6161642,20.125 C12.6161642,20.6082492 13.0082194,21 13.4918439,21 C19.2953383,21 24,16.2989898 24,10.5 C24,4.70101019 19.2953383,0 13.4918439,0 Z"
+                    id="Path"></path>
+                </g>
               </g>
             </g>
           </g>
-        </g>
-      </svg>
+        </svg>
+      </a>
 
 
 
@@ -310,16 +312,20 @@ swiper-container {
 }
 
 .swiper-tinder-label {
-  @apply absolute text-white text-2xl font-bold;
+  position: absolute;
+  /* Changed from absolute to fixed */
+  top: 20px;
+  font-weight: bold;
+  transition: opacity 0.3s;
+  z-index: 10;
+  pointer-events: none;
+  background-color: rgba(0, 0, 0, 0.7);
+  padding: 5px 10px;
+  border-radius: 5px;
+  color: white;
 }
 
-.swiper-tinder-label-yes {
-  @apply top-4 right-4 text-green-500 text-right w-1/2;
-}
 
-.swiper-tinder-label-no {
-  @apply top-4 right-4 text-red-500 text-left w-1/2;
-}
 
 /* Assuming .cards-wrapper is the parent of .card-container */
 swiper-slide {
@@ -383,15 +389,7 @@ swiper-slide {
   transform: rotateY(180deg);
 }
 
-.swiper-tinder-label {
-  position: absolute;
-  top: 20px;
-  font-size: 24px;
-  font-weight: bold;
-  opacity: 0;
-  transition: opacity 0.3s ease;
-  z-index: 10;
-}
+
 
 .swiper-tinder-label.visible {
   opacity: 1;
@@ -399,30 +397,7 @@ swiper-slide {
 
 
 
-.swiper-tinder-label {
-  position: absolute;
-  font-weight: bold;
-  padding: 4px 12px;
-  text-transform: uppercase;
-  border-radius: 4px;
-  opacity: 0;
-}
 
-.swiper-tinder-label-yes {
-  @apply bg-green-500;
-  left: 3%;
-  top: 5%;
-  transform: rotate(-20deg);
-  transform-origin: right top;
-}
-
-.swiper-tinder-label-no {
-  @apply bg-red-500;
-  right: 3%;
-  top: 5%;
-  transform: rotate(20deg);
-  transform-origin: left top;
-}
 
 .swiper-tinder-label {
   position: absolute;
@@ -436,32 +411,15 @@ swiper-slide {
 }
 
 
-.swiper-tinder-label-no {
-  background-color: red;
-  left: 10px;
-  top: 10px;
-}
+
 
 .swiper-tinder-button-hidden {
   display: none;
 }
 
-.swiper-tinder-label {
-  position: absolute;
-  top: 20px;
-  font-size: var(--swiper-tinder-label-font-size, 32px);
-  font-weight: bold;
-  color: var(--swiper-tinder-label-text-color, #fff);
-  opacity: 0;
-  transition: opacity 0.3s;
-  z-index: 1;
-}
 
 
-.swiper-tinder-label-no {
-  left: 20px;
-  @apply text-red-500 bg-red-200
-}
+
 
 .swiper-tinder-label {
   position: absolute;
@@ -510,16 +468,6 @@ swiper-slide {
   z-index: 2;
 }
 
-
-.swiper-tinder-label {
-  position: absolute;
-  top: 20px;
-  font-weight: bold;
-  // opacity: 0;
-  transition: opacity 0.3s;
-  z-index: 10;
-  pointer-events: none;
-}
 
 
 
