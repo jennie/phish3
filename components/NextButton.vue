f705a3b53d3dc6d6dbd20529c75d6d1020a1fdb4
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-07-24 19:18:48 -0400

    Make buttons components

---
+++ components/NextButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <svg width="56px" height="70px" viewBox="0 0 56 70" version="1.1" xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>icon-next</title>
+      <g id="UX-Flow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Scenerio-00---00" transform="translate(-202, -580)">
+          <g id="icon-next" transform="translate(202, 580)">
+            <text id="NEXT" font-family="Roboto-Medium, Roboto" font-size="10" font-weight="400" fill="#fff">
+              <tspan x="15.4243164" y="68">NEXT</tspan>
+            </text>
+            <g id="button" transform="translate(28, 28) scale(-1, 1) translate(-28, -28)">
+              <circle id="button-back" stroke="#000000" stroke-width="3" fill="#FFFFFF" cx="28" cy="28" r="26.5">
+              </circle>
+              <path
+                d="M13,27.726715 C13,26.9935424 13.5980123,26.393674 14.3289163,26.393674 L38.1663523,26.393674 L32.3025091,20.5116306 C31.7875541,19.9950772 31.7875541,19.1452635 32.3025091,18.6287102 C32.8174642,18.1121568 33.6646483,18.1121568 34.1796034,18.6287102 L42.3192157,26.7935863 C42.5683875,27.0435315 42.7012791,27.3767917 42.7012791,27.743378 C42.7012791,28.1099643 42.5683875,28.4432245 42.3192157,28.6931697 L34.1796034,36.8580458 C33.9138201,37.124654 33.5815911,37.2412951 33.2327505,37.2412951 C32.88391,37.2412951 32.5516809,37.107991 32.2858977,36.8580458 C31.7709426,36.3414924 31.7709426,35.4916788 32.2858977,34.9751254 L38.1497408,29.093082 L14.3289163,29.093082 C13.5980123,29.043093 13,28.4598875 13,27.726715 Z"
+                id="arrow-right" fill="#000000"
+                transform="translate(27.8506, 27.7413) scale(-1, 1) translate(-27.8506, -27.7413)"></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
