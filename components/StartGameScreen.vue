b7c77b1776d667dfad39186a28139a3926c77b57
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-09-18 10:03:11 -0400

    Adjust layout and add title in StartGameScreen component

--- components/StartGameScreen.vue
+++ components/StartGameScreen.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="start-screen h-screen flex flex-col justify-center items-center  text-blue-100">
     <img src="/images/reelPolytechnicLogo.png" alt="Reel Polytechnic Logo" class=" max-w-72 mb-4" />
+    <h2 class="text-3xl text-blue-50 uppercase font-bold mb-2">Cybersecurity Trainer</h2>
     <div v-if="isLoading" class="loading-container w-full max-w-md px-4 text-center">
       <p class="text-2xl mb-4">Loading...</p>
       <div class="flex justify-center items-center">
@@ -13,14 +14,15 @@
       <AuthState v-slot="{ loggedIn, user, clear }">
         <div v-if="loggedIn" class="text-center">
           <p class="text-xl mb-2">Welcome, {{ user.email }}!</p>
-          <button @click="logout"
-            class="uppercase text-blue-300 underline text-sm font-semibold transition-colors mb-2">
-            Logout
-          </button>
+
           <p class="mb-4" v-if="user.bestScore > 0">Your best score: {{ bestScorePercentage }}%</p>
           <button @click="startGame"
             class="bg-blue-200 text-zinc-800 rounded-full px-12 py-3 text-lg font-semibold hover:bg-blue-300 transition-colors mb-4">
             Start Game
+          </button>
+          <button @click="logout"
+            class="block text-center mx-auto uppercase text-blue-300 underline text-sm font-semibold transition-colors mb-2">
+            Logout
           </button>
 
         </div>
