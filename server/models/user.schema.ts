afe9fca2d4d86ea8cbb4e06f7d7faf94549151be
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-10-09 09:00:35 -0400

    Refactor user schema to add 'external' as a valid user type

--- server/models/user.schema.ts
+++ server/models/user.schema.ts
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   type: {
     type: String,
-    enum: ["student", "employee", "governor", "gammaspace"],
+    enum: ["student", "employee", "governor", "gammaspace", "external"],
     required: true,
   },
   bestScore: { type: Number, default: 0 },
